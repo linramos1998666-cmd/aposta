0ef21a5cf5e3a7354b31cac13119231bb2b13402
ULi6 <linramos1998666@gmail.com>
2026-03-23 17:38:42 -0300

    Update odds.js

--- api/odds.js
+++ api/odds.js
@@ -1,1 +1,19 @@
+export default async function handler(req) {
 
+  const url = "https://api.the-odds-api.com/v4/sports/soccer/odds/?regions=eu&markets=h2h&apiKey=c652908a94bc3cc4a43c631260dcc724";
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return new Response(JSON.stringify(data), {
+      headers: { "Content-Type": "application/json" },
+      status: 200
+    });
+
+  } catch (error) {
+    return new Response(JSON.stringify({ error: "Erro na API" }), {
+      status: 500
+    });
+  }
+}
